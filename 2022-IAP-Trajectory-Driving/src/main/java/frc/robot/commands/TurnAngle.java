a03bf5ee715389123d118d04c967603a135573f8
Hibspliddu <ibraheems.wvhs@gmail.com>
2022-12-17 12:36:48 -0800

    Created Turn Angle

--- 2022-IAP-Trajectory-Driving/src/main/java/frc/robot/commands/TurnAngle.java
+++ 2022-IAP-Trajectory-Driving/src/main/java/frc/robot/commands/TurnAngle.java
@@ -1,35 +1,51 @@
 package frc.robot.commands;
+
 import edu.wpi.first.math.trajectory.*;
 import edu.wpi.first.math.trajectory.constraint.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.DriveTrain;
 
-public class DistanceAuto extends CommandBase{
-    private final DriveTrain driveTrain;
-    private double pos;
-    private PIDController pid;
 
-    public DistanceAuto(DriveTrain dt, double pos){
+
+public class TurnAngle extends CommandBase{
+    private final DriveTrain driveTrain;
+    private double angle;
+    private PIDController pid;
+    public double error;
+
+    public TurnAngle(DriveTrain dt, double angle){
         //use addRequirements() here to declare subsystem dependencies
         driveTrain = dt;
-        this.pos = pos;
+        this.angle = angle;
         addRequirements(driveTrain);
     }
 
     public void initialize(){
         driveTrain.resetEncoders();
         pid = new PIDController(Constants.PIDConstants.kP, Constants.PIDConstants.kI, Constants.PIDConstants.kD);
-        pid.setSetpoint(pos);
+        pid.setSetpoint(angle);
     }
 
     public void execute(){
-        pid.calculate((driveTrain.getLeftPos()+driveTrain.getRightPos())/2);
+        error = Math.abs(angle - ((driveTrain.getAngle());
+        if(angle>= 180 && Math.abs(angle - ((driveTrain.getAngle()))) > 0.1){
+            driveTrain.tankDrive(0.3, -0.3);
+        }else if(angle< 180 && Math.abs(angle - ((driveTrain.getAngle()))) > 0.1){
+            driveTrain.tankDrive(-0.3, 0.3);
+        }else{
+            driveTrain.tankDrive(0, 0);
+        }
+        pid.calculate(driveTrain.getAngle());
+        SmartDashboard.putNumber("angle: ", angle);
     }
 
     @Override
     public boolean isFinished(){
-        return Math.abs(pos - ((driveTrain.getLeftPos()+driveTrain.getRightPos())/2)) < 0.1;
+        return (error < 0.1);
     }
+}
+
 }
